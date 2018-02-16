df1bee78ab8c57389aba896072da24ef645c8d0d
egjimenezg <egjimenezg@gmail.com>
2018-02-16 14:14:48 -0600

    Adding an example with Flesk

--- src/app/run.py
+++ src/app/run.py
@@ -1,5 +1,15 @@
-def main():
-    print "This is the main function that is called"
+from flask import Flask
 
-if __name__ == "main":
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    html = "<h3>Adding the title for the route</h3>"
+    return html.format()
+
+def main():
+    app.run(host='0.0.0.0', port=80)
+
+if __name__ == '__main__':
     main()
